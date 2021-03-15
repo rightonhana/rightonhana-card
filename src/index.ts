b4c67de7585f9e481d21f096694149ac904c9502
Maria Florencia Gonzalez <rightonhana@gmail.com>
2021-03-15 16:40:26 -0700

    :sparkles: create index.ts for personal card

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+import {
+  bold,
+  cyanText,
+  mix,
+  blueText,
+  redText,
+  underlined,
+  yellowText,
+} from "@vangware/forcli";
+import boxen from "boxen";
+import clear from "clear";
+
+const link = mix([cyanText, underlined]);
+const name = mix([cyanText, bold]);
+
+const message = `
+Hello there!
+
+I'm ${name("Florencia Gonzalez")} / ${name("@rightonhana")}
+
+${[
+  "⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻⣿⣿",
+  "⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿⣿",
+  "⣿⣿⣿⡀⣿⣿⣿⣧⢻⣿⣶⣷⣿⣿⣿⣿⣿⣿⠿⠶⡝⠀⣿⣿",
+  "⣿⣿⣿⣷⠘⣿⣿⣿⢏⣿⣿⣋⣀⣈⣻⣿⣿⣷⣤⣤⣿⡐⢿⣿",
+  "⣿⣿⣿⣿⣆⢩⣝⣫⣾⣿⣿⣿⣿⡟⠿⠿⠦⠀⠸⠿⣻⣿⡄⢻",
+  "⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⠇⣼",
+  "⣿⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰⣿",
+  "⣿⣿⣿⣿⣿⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣿⣿",
+  "⣿⣿⣿⣿⣿⠏⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿",
+  "⣿⣿⣿⣿⠟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿",
+  "⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿",
+  "⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿",
+  "⣿⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⢸⣿",
+  "⡏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿",
+]
+  .map(cyanText)
+  .join("\n")}
+
+I'm a Front-end Developer with experience in Angular, React and Svelte.
+I love cats and my hobbies are playing games and learn new technologies.
+I'm from ${cyanText("Ar")}ge${yellowText("n")}ti${cyanText(
+  "na"
+)} but I'm currently living in the ${redText("U")}${blueText("S")}A.
+
+${bold("Pronouns")}: She / Her
+
+
+If you want to know more about me:
+
+${bold("Github")}: ${link("https://github.com/rightonhana")}
+${bold("NPM")}: ${link("https://www.npmjs.com/~rightonhana")}
+${bold("Website")}: ${link("https://rightonhana.me/")}
+`;
+
+export default [
+  clear(),
+  console.log(
+    boxen(message, {
+      align: "center",
+      borderColor: "cyan",
+      borderStyle: "double",
+      float: "center",
+      padding: 1,
+    })
+  ),
+];
