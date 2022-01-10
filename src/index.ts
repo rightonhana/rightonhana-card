a0dbc9941223eae9aec187a8e9edb7d5d81a7e16
Maria Florencia Gonzalez <rightonhana@gmail.com>
2022-01-09 16:04:08 -0800

    :art: format code and update ansi usage

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import {
-	bold,
-	foregroundBlue,
-	foregroundCyan,
-	foregroundRed,
-	foregroundYellow,
-	mix,
-	underlined
+  bold,
+  foregroundBlue,
+  foregroundCyan,
+  foregroundRed,
+  foregroundYellow,
+  mix,
+  underlined,
 } from "@vangware/ansi";
 import boxen from "boxen";
 import clear from "clear";
@@ -19,65 +19,64 @@ const isWindows = process.platform === "win32";
 const message = `
 Hello there!
 
-I'm ${name("Florencia Gonzalez")} / ${name("@rightonhana")}
+I'm ${name`Florencia Gonzalez`} / ${name`@rightonhana`}
 
-${(isWindows ?
-	[
-		"  ▐▀▄      ▄▀▌    ▄▄▄▄▄▄▄",
-		"  ▌▒▒▀▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄",
-		" ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄",
-		" ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄",
-		"▀█▒▒█▌▒▒█▒▒▐█▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌",
-		"▀▌▒▒▒▒▒▀▒▀▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐ ▄▄",
-		"▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄█▒█",
-		"▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀",
-		" ▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌",
-		"     ▀▄▄▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀"
-	]
-	: [
-		"⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻⣿⣿",
-		"⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿⣿",
-		"⣿⣿⣿⡀⣿⣿⣿⣧⢻⣿⣶⣷⣿⣿⣿⣿⣿⣿⠿⠶⡝⠀⣿⣿",
-		"⣿⣿⣿⣷⠘⣿⣿⣿⢏⣿⣿⣋⣀⣈⣻⣿⣿⣷⣤⣤⣿⡐⢿⣿",
-		"⣿⣿⣿⣿⣆⢩⣝⣫⣾⣿⣿⣿⣿⡟⠿⠿⠦⠀⠸⠿⣻⣿⡄⢻",
-		"⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⠇⣼",
-		"⣿⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰⣿",
-		"⣿⣿⣿⣿⣿⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣿⣿",
-		"⣿⣿⣿⣿⣿⠏⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿",
-		"⣿⣿⣿⣿⠟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿",
-		"⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿",
-		"⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿",
-		"⣿⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⢸⣿",
-		"⡏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿",
-	])
-		.map(foregroundCyan)
-		.join("\n")}
+${(isWindows
+  ? [
+      "  ▐▀▄      ▄▀▌    ▄▄▄▄▄▄▄",
+      "  ▌▒▒▀▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄",
+      " ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄",
+      " ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄",
+      "▀█▒▒█▌▒▒█▒▒▐█▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌",
+      "▀▌▒▒▒▒▒▀▒▀▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐ ▄▄",
+      "▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄█▒█",
+      "▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀",
+      " ▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌",
+      "     ▀▄▄▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀",
+    ]
+  : [
+      "⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻⣿⣿",
+      "⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿⣿",
+      "⣿⣿⣿⡀⣿⣿⣿⣧⢻⣿⣶⣷⣿⣿⣿⣿⣿⣿⠿⠶⡝⠀⣿⣿",
+      "⣿⣿⣿⣷⠘⣿⣿⣿⢏⣿⣿⣋⣀⣈⣻⣿⣿⣷⣤⣤⣿⡐⢿⣿",
+      "⣿⣿⣿⣿⣆⢩⣝⣫⣾⣿⣿⣿⣿⡟⠿⠿⠦⠀⠸⠿⣻⣿⡄⢻",
+      "⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⠇⣼",
+      "⣿⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰⣿",
+      "⣿⣿⣿⣿⣿⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣿⣿",
+      "⣿⣿⣿⣿⣿⠏⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿",
+      "⣿⣿⣿⣿⠟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿",
+      "⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿",
+      "⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿",
+      "⣿⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⢸⣿",
+      "⡏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿",
+    ]
+)
+  .map(foregroundCyan)
+  .join("\n")}
 
 I'm a Front-end Developer with experience in Angular, React and Svelte.
 I love cats and my hobbies are playing games and learn new technologies.
-I'm from ${foregroundCyan("Ar")}ge${foregroundYellow("n")}ti${foregroundCyan(
-	"na"
-)} but I'm currently living in the ${foregroundRed("U")}${foregroundBlue("S")}A.
+I'm from ${foregroundCyan`Ar`}ge${foregroundYellow`n`}ti${foregroundCyan`na`} but I'm currently living in the ${foregroundRed`U`}${foregroundBlue`S`}A.
 
-${bold("Pronouns")}: She / Her
+${bold`Pronouns`}: She / Her
 
 
 If you want to know more about me:
 
-${bold("Github")}: ${link("https://github.com/rightonhana")}
-${bold("NPM")}: ${link("https://www.npmjs.com/~rightonhana")}
-${bold("Website")}: ${link("https://rightonhana.me/")}
+${bold`Github`}: ${link`https://github.com/rightonhana`}
+${bold`NPM`}: ${link`https://www.npmjs.com/~rightonhana`}
+${bold`Website`}: ${link`https://rightonhana.me/`}
 `;
 
 export default [
-	clear(),
-	console.log(
-		boxen(message, {
-			align: "center",
-			borderColor: "cyan",
-			borderStyle: "double",
-			float: "center",
-			padding: 1,
-		})
-	),
+  clear(),
+  console.log(
+    boxen(message, {
+      align: "center",
+      borderColor: "cyan",
+      borderStyle: "double",
+      float: "center",
+      padding: 1,
+    })
+  ),
 ];
